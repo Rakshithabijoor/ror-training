7152b1847c2ebd0f2be6aea6a44e0062b3c6f8ee
rbijoor <rbijoor@C02F2T1JMD6M.local>
2022-06-21 17:01:04 +0530

    ruby

--- myblog/app/models/post.rb
+++ myblog/app/models/post.rb
@@ -1,7 +1,13 @@
+require "module/wordcount.rb"
 class Post < ApplicationRecord
+  include Wordcount
   validates :title, :body, :status, presence: true
   validates :title, length: { maximum: 10 }
   belongs_to :user
   has_many :posttags
   has_many :tags ,through: :posttags
+  STATUS={0=>"draft",1=>"publish",2=>"archive"}
+  scope :published, ->  { where(status: 1) }
+  scope :drafted, ->  { where(status: 0) }
+  scope :archived, ->  { where(status: 2) }
 end
---
+++ myblog/app/models/wordcount.rb
@@ -0,0 +1,6 @@
+module Wordcount
+  def count_word
+  self.body.length
+  end
+  def is_bad?(paragraph)
+end
